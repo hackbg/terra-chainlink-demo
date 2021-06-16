bf69a62db338230f5c6538d16432b5a7860e28bd
denis <denismaximov98@gmail.com>
2021-06-16 20:56:42 +0300

    wip: new hash of ea submodule, env for adapter

--- scripts/bash/run-adapter.sh
+++ scripts/bash/run-adapter.sh
@@ -5,6 +5,6 @@ set -e
 echo "*** Build Adapter ***"
 cd $(dirname ${BASH_SOURCE[0]})/../..
 cd ./external-adapters
-docker build -t terrademo/terra-external-adapter --build-arg adapter=terra .
+#docker build -t terrademo/terra-external-adapter --build-arg adapter=terra .
 cd ..
 docker-compose up -d external-adapter
