873b538abdd01139967c78cc169c80894ee8025d
Evangelos Barakos <e.mparakos@gmail.com>
2021-07-23 19:23:52 +0300

    Removed old scripts

--- scripts/bash/run-chainlink.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "*** Run Chainlink nodes ***"
-cd $(dirname ${BASH_SOURCE[0]})/../..
-
-docker-compose up -d chainlink-node-1 chainlink-node-2 chainlink-node-3
--- scripts/bash/run-ei.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "*** Run External Initiator ***"
-
-cd $(dirname ${BASH_SOURCE[0]})/../..
-
-docker-compose up -d external-initiator-1
