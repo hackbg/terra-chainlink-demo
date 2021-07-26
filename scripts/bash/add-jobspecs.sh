e2a64a0eff7ccd7add82532ec1090a40f01d7827
Evangelos Barakos <e.mparakos@gmail.com>
2021-07-26 17:25:54 +0300

    Missing 3rd oracle + removed duplication in jobspecs

--- scripts/bash/add-jobspecs.sh
+++ scripts/bash/add-jobspecs.sh
@@ -1,15 +1,11 @@
+
 #!/bin/bash
-
 source ./scripts/bash/common.sh
-
 echo "Adding JobSpecs to Chainlink node..."
 
-CL_URL="http://localhost:6691"
-
-login_cl "$CL_URL"
-
-payload=$(
-cat <<EOF
+function jobspec() {
+echo $(
+cat << EOF
 {
   "initiators": [
     {
@@ -19,12 +15,12 @@ cat <<EOF
         "body": {
           "endpoint": "terra",
           "contract_address": "terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6",
-          "account_address": "terra1757tkx08n0cqrw7p86ny9lnxsqeth0wgp0em95",
+          "account_address": "$1",
           "fluxmonitor": {
             "requestData": {
               "data": { "from": "LUNA", "to": "USD" }
             },
-            "feeds": [{ "url": "http://price-adapter-1:8080" }, { "url": "http://price-adapter-3:8080" }],
+            "feeds": [{ "url": "http://price-adapter-1:8080" }, { "url": "http://price-adapter-2:8080" },  { "url": "http://price-adapter-3:8080" }],
             "threshold": 0.1,
             "absoluteThreshold": 0,
             "precision": 8,
@@ -37,104 +33,41 @@ cat <<EOF
   ],
   "tasks": [
     {
-      "type": "terra-adapter1",
+      "type": "$2",
       "params": {}
     }
   ]
 }
 EOF
 )
+}
 
-JOBID=$(curl -s -b ./cookiefile -d "$payload" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/specs")
+CL_URL="http://localhost:6691"
+login_cl "$CL_URL"
+ORACLE_ADDRESS="terra1757tkx08n0cqrw7p86ny9lnxsqeth0wgp0em95"
+BRIDGE_NAME="terra-adapter1"
+
+JOBID=$(curl -s -b ./cookiefile -d "$(jobspec $ORACLE_ADDRESS $BRIDGE_NAME)" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/specs")
 echo $JOBID
+
 
 CL_URL="http://localhost:6692"
-
 login_cl "$CL_URL"
+ORACLE_ADDRESS="terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp"
+BRIDGE_NAME="terra-adapter2"
 
-payload2=$(
-cat <<EOF
-{
-  "initiators": [
-    {
-      "type": "external",
-      "params": {
-        "name": "terra",
-        "body": {
-          "endpoint": "terra",
-          "contract_address": "terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6",
-          "account_address": "terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp",
-          "fluxmonitor": {
-            "requestData": {
-              "data": { "from": "LUNA", "to": "USD" }
-            },
-            "feeds": [{ "url": "http://price-adapter-1:8080" }, { "url": "http://price-adapter-3:8080" }],
-            "threshold": 0.1,
-            "absoluteThreshold": 0,
-            "precision": 8,
-            "pollTimer": { "period": "30s" },
-            "idleTimer": { "duration": "1m" }
-          }
-        }
-      }
-    }
-  ],
-  "tasks": [
-    {
-      "type": "terra-adapter2",
-      "params": {}
-    }
-  ]
-}
-EOF
-)
-
-JOBID=$(curl -s -b ./cookiefile -d "$payload2" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/specs")
+JOBID=$(curl -s -b ./cookiefile -d "$(jobspec $ORACLE_ADDRESS $BRIDGE_NAME)" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/specs")
 echo $JOBID
 
+
 CL_URL="http://localhost:6693"
-
 login_cl "$CL_URL"
+ORACLE_ADDRESS="terra199vw7724lzkwz6lf2hsx04lrxfkz09tg8dlp6r"
+BRIDGE_NAME="terra-adapter3"
 
-payload3=$(
-cat <<EOF
-{
-  "initiators": [
-    {
-      "type": "external",
-      "params": {
-        "name": "terra",
-        "body": {
-          "endpoint": "terra",
-          "contract_address": "terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6",
-          "account_address": "terra1757tkx08n0cqrw7p86ny9lnxsqeth0wgp0em95",
-          "fluxmonitor": {
-            "requestData": {
-              "data": { "from": "LUNA", "to": "USD" }
-            },
-            "feeds": [{ "url": "http://price-adapter-1:8080" }, { "url": "http://price-adapter-3:8080" }],
-            "threshold": 0.1,
-            "absoluteThreshold": 0,
-            "precision": 8,
-            "pollTimer": { "period": "30s" },
-            "idleTimer": { "duration": "1m" }
-          }
-        }
-      }
-    }
-  ],
-  "tasks": [
-    {
-      "type": "terra-adapter3",
-      "params": {}
-    }
-  ]
-}
-EOF
-)
-
-JOBID=$(curl -s -b ./cookiefile -d "$payload3" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/specs")
+JOBID=$(curl -s -b ./cookiefile -d "$(jobspec $ORACLE_ADDRESS $BRIDGE_NAME)" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/specs")
 echo $JOBID
 
 
 echo "Jobspecs has been added to Chainlink nodes"
+
--- scripts/terrajs/src/index.js
+++ scripts/terrajs/src/index.js
@@ -18,6 +18,7 @@ const FLUX_PATH =
 const ORACLES = [
   "terra1757tkx08n0cqrw7p86ny9lnxsqeth0wgp0em95",
   "terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp",
+  "terra199vw7724lzkwz6lf2hsx04lrxfkz09tg8dlp6r"
 ];
 
 const mk = new MnemonicKey({
