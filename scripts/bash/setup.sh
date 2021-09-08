33355e6c251c316bc90e7fe591221665f49be8fa
denismaxim0v <denis_maxim0v@protonmail.com>
2021-09-08 15:26:59 +0300

    feat: add kafka, pseudo config feeder for metrics"

--- scripts/bash/setup.sh
+++ scripts/bash/setup.sh
@@ -13,15 +13,24 @@ echo "{}" > ./scripts/terrajs/addresses.json
 echo "Waiting for localterra services to be ready"
 sleep 6
 
+docker-compose up -d pseudo-config
+
 ./scripts/bash/setup-feeds.sh $1
 
 ./scripts/bash/setup-operators.sh
 
-echo "Waiting a bit for the answer to be written on-chain"
-sleep 60
+
+docker-compose up -d zookeeper
+docker-compose up -d kafka
+
+sleep 10
+docker-compose exec kafka kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic Terra
 
 docker-compose up -d prometheus
 docker-compose up -d terra-chainlink-exporter
+docker-compose up -d consumer
+
+echo "Waiting a bit for the answer to be written on-chain"
+sleep 30
 
 ./scripts/bash/check-answer.sh
-
